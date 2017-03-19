f2118c23bce6b4f26a8307cac05cfb83a825a4be
PatrickSoueida <soueidapatrick@gmail.com>
2017-03-19 01:12:37 -0400

    More level design
    Added more things to level design. Added MoveTheBlocks script but not
    implemented yet. Still a work in progress.

---
+++ Assets/Scripts/MoveTheBlocks.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTheBlocks : MonoBehaviour {
+
+	[SerializeField]
+	Mesh m;
+
+	[SerializeField]
+	Transform block;
+
+	[SerializeField]
+	Transform startTransform;
+
+	[SerializeField]
+	Transform endTransform;
+
+	[SerializeField]
+	float movementSpeed;
+
+	private Rigidbody rbBlock;
+	private Vector3 direction;
+	private Transform destination;
+
+	void Start()
+	{
+		rbBlock = block.GetComponent<Rigidbody> ();
+		setDestination (startTransform);
+	}
+
+	void FixedUpdate() 
+	{
+		rbBlock.MovePosition (block.position + direction * movementSpeed * Time.fixedDeltaTime);
+
+		if (Vector3.Distance (block.position, destination.position) < movementSpeed * Time.fixedDeltaTime) 
+		{
+			setDestination (destination == startTransform ? endTransform : startTransform);
+		}
+	}
+
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.green;
+		Gizmos.DrawMesh(m, startTransform.position, block.rotation, block.localScale);
+
+		Gizmos.color = Color.red;
+		Gizmos.DrawMesh(m, endTransform.position, block.rotation, block.localScale);
+
+	}
+
+	void setDestination(Transform d)
+	{
+		destination = d;
+		direction = (destination.position - block.position).normalized;
+	}
+}
