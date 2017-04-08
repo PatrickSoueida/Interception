83cdcc2d2d3a9a6e5ec6fad7ccd525533c33127e
Jordan Orsini <jordan__o@hotmail.com>
2017-04-07 21:32:02 -0400

    ok

---
+++ Assets/Asset Store/Main Menu Scripts/MouseHover.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHover : MonoBehaviour {
+    public Color colorStart;
+    public Color colorEnd = Color.green;
+    public Renderer rend; 
+
+	// Use this for initialization
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        colorStart = rend.material.color;
+    }
+
+    void OnMouseEnter()
+    {
+        rend.material.color = colorEnd;
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = colorStart;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
