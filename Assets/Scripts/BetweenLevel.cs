4eb3464bb5c62fdc03bb8a2d90ef486d3ef61345
Jordan Orsini <jordan__o@hotmail.com>
2017-04-11 12:57:00 -0400

    loading screen and outro ui implemented

---
+++ Assets/Scripts/BetweenLevel.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BetweenLevel : MonoBehaviour {
+
+    public GameObject loadingScreen;
+    public AudioSource startSound;
+    AudioSource myStartSound;
+
+    void Start()
+    {
+        myStartSound = startSound.GetComponent<AudioSource>();
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void play()
+	{
+        Instantiate(myStartSound);
+        transform.gameObject.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        Time.timeScale = 1;
+        loadingScreen.SetActive(true);
+	}
+}
